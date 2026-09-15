0d6ca7e39d466386d3e0b6f2fb80fd87ac1e7433
aditijain18-cse <jainaditi1803@gmail.com>
2026-09-16 00:20:20 +0530

    Add files via upload

---
+++ q17.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    float d, root1, root2;
+
+    printf("Enter a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0)
+    {
+        printf("Not a quadratic equation");
+    }
+    else
+    {
+        d = b * b - 4 * a * c;
+
+        printf("Discriminant = %.2f\n", d);
+
+        if (d > 0)
+        {
+            root1 = (-b + sqrt(d)) / (2 * a);
+            root2 = (-b - sqrt(d)) / (2 * a);
+
+            printf("Roots are real and distinct\n");
+            printf("Root 1 = %.2f\n", root1);
+            printf("Root 2 = %.2f\n", root2);
+        }
+        else if (d == 0)
+        {
+            root1 = -b / (2 * a);
+
+            printf("Roots are real and equal\n");
+            printf("Root 1 = Root 2 = %.2f\n", root1);
+        }
+        else
+        {
+            printf("Roots are imaginary\n");
+        }
+    }
+
+    return 0;
+}
